c88bc57853c52ab8d3bf43b2d385a2433ebac4fb
kauzu <kauzu@posteo.de>
2025-07-27 13:22:37 +0200

    fix mac setup

--- custom_components/wp6003/bluetooth.py
+++ custom_components/wp6003/bluetooth.py
@@ -1,8 +1,4 @@
-from homeassistant.components.bluetooth import (
-    async_register_callback,
-)
-from .ble_decoder import parse_wp6003_ble_packet
-from .const import DOMAIN, CONF_MAC
+from .const import CONF_MAC
 
 async def async_setup_entry(hass, config_entry, async_add_entities=None):
     target_mac = config_entry.data[CONF_MAC].lower()
@@ -15,6 +11,6 @@ async def async_setup_entry(hass, config_entry, async_add_entities=None):
         if payload:
             data = parse_wp6003_ble_packet(payload)
             if data:
-                hass.bus.fire(f"{DOMAIN}_update", data)
+                hass.bus.fire("wp6003_update", data)
 
     async_register_callback(hass, ble_callback, {"manufacturer_id": 0xEB01})
--- custom_components/wp6003/config_flow.py
+++ custom_components/wp6003/config_flow.py
@@ -1,21 +1,16 @@
 from homeassistant import config_entries
-from homeassistant.const import CONF_MAC
 import voluptuous as vol
-from .const import DOMAIN
+from .const import DOMAIN, CONF_MAC
 
 class WP6003ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
-        errors = {}
-
         if user_input is not None:
-            return self.async_create_entry(title="WP6003 BLE Sensor", data=user_input)
-
-        schema = vol.Schema({
-            vol.Required(CONF_MAC): str
-        })
+            return self.async_create_entry(
+                title="WP6003 BLE Sensor",
+                data=user_input  # stores {"mac_address": "..."}
+            )
 
         return self.async_show_form(
             step_id="user",
-            data_schema=schema,
-            errors=errors
+            data_schema=vol.Schema({vol.Required(CONF_MAC): str}),
         )
