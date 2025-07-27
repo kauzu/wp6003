8bd4ab992548bd911fa8678b0dfe33e690b6a76c
kauzu <kauzu@posteo.de>
2025-07-27 13:13:21 +0200

    avoid restart and stuff

--- custom_components/wp6003/__init__.py
+++ custom_components/wp6003/__init__.py
@@ -10,3 +10,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     from . import bluetooth
     await bluetooth.async_setup_entry(hass, entry)
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    # Cleanup listeners, stop threads, etc.
+    return True
+
+async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    await async_unload_entry(hass, entry)
+    await async_setup_entry(hass, entry)
